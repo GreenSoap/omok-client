010d347c94d8d4e2f72510d444cd158543bf087f
greensoap <william.sjokvist@gmail.com>
2022-03-09 13:31:44 +0100

    Fixed issue rendering board lines

---
+++ src/gui/omok_piece.ts
@@ -0,0 +1,47 @@
+import type p5 from "p5";
+
+export enum OmokPieceState {
+  EMPTY,
+  PLAYER_1_PIECE,
+  PLAYER_2_PIECE
+}
+
+export class OmokPiece {
+  x: number; 
+  y: number;
+  size: number;
+  pieceNumber: number;
+  p: p5;
+  state: OmokPieceState = OmokPieceState.EMPTY;
+
+  constructor (p: p5, x: number, y: number, size: number){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.p = p;
+  }
+
+  draw(){
+    this.p.stroke(0);
+    this.p.line(this.x+this.size/2, this.y, this.x+this.size/2, this.y+this.size);
+
+    this.p.stroke(0);
+    this.p.line(this.x, this.y+this.size/2, this.x+this.size, this.y+this.size/2);
+  }
+
+  clicked(is_player_one = true){
+    if (this.state != OmokPieceState.EMPTY) 
+      return false;
+    
+    this.state = (is_player_one) ? OmokPieceState.PLAYER_1_PIECE : OmokPieceState.PLAYER_2_PIECE;
+    this.p.fill((is_player_one) ? 0 : 255);
+    this.p.circle(this.x+this.size/2, this.y+this.size/2, this.size/2);
+    return true;
+  }
+
+  is_within_boundary(x: number, y: number){
+    return (
+    (x < this.x+this.size) && (x > this.x) && 
+    (y < this.y+this.size) && (y > this.y));
+  }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,58 +1,29 @@
 <script lang="ts">
   import p5 from "p5";
-
-  class OmokPiece {
-    x: number; 
-    y: number;
-    size: number;
-    pieceNumber: number;
-    color: p5.Color;
-    p: p5;
-
-    constructor (p: p5, x: number, y: number, size: number){
-      this.x = x;
-      this.y = y;
-      this.size = size;
-      this.p = p;
-      this.color = this.p.color(255, 100);
-    }
-
-    draw(){
-      this.p.fill(this.color);
-      this.p.stroke(255, 0);
-      this.p.rect(this.x, this.y, this.size, this.size);
-
-      this.p.stroke(0);
-      this.p.line(this.x+this.size/2, this.y, this.x+this.size/2, this.y+this.size);
-
-      this.p.stroke(0);
-      this.p.line(this.x, this.y+this.size/2, this.x+this.size, this.y+this.size/2);
-    }
-
-    clicked(){
-      this.p.fill(0);
-      this.p.circle(this.x+this.size/2, this.y+this.size/2, this.size/2);
-    }
-
-    is_within_boundary(x: number, y: number){
-      return (
-      (x < this.x+this.size) && (x > this.x) && 
-      (y < this.y+this.size) && (y > this.y));
-    }
+  import { OmokPiece, OmokPieceState } from '../gui/omok_piece';
+  
+  enum OmokBoardState {
+    EMPTY,
+    PLAYER_1_VICTORY,
+    PLAYER_2_VICTORY,
+    DRAW
   }
-
-  //const omok_board = new OmokBoard(5, 50);
 
   const omok_board = (p: p5) => {
     const board_side_pieces: number = 5;
     const omok_piece_size: number = 100;
-    const omok_piece_radius = new p5.Vector(omok_piece_size, omok_piece_size);
+    const board_state: OmokBoardState = OmokBoardState.EMPTY;
 
+    const omok_piece_radius = new p5.Vector(omok_piece_size, omok_piece_size);
     const pieces: Array<Array<OmokPiece>> = [];
+  
+    let is_player_one_turn = true;
+
     p.setup = () => {
-      p.noStroke();
       p.createCanvas(700, 700);
+      p.background(234, 221, 202);
       p.frameRate(0);
+
       let pieceNumber = 1;
       for (let x = 0; x < board_side_pieces; x++){
         pieces[x] = [];
@@ -66,20 +37,23 @@
     }
 
     p.draw = () => {
-      p.strokeWeight(1);
+      p.strokeWeight(2);
       for (let x = 0; x < board_side_pieces; x++)
         for (let y = 0; y < board_side_pieces; y++)
           pieces[x][y].draw();
-
-      p.strokeWeight(4);
-      p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
     }
 
-    p.mouseClicked = (event: any) => {        
+    p.mouseClicked = (event: any) => {       
       for (let x = 0; x < board_side_pieces; x++){
         for (let y = 0; y < board_side_pieces; y++){
           if (pieces[x][y].is_within_boundary(event.x, event.y)){
-            pieces[x][y].clicked();
+            const did_place_piece = pieces[x][y].clicked(is_player_one_turn);
+
+            if (did_place_piece){
+              //Check if move led to victory
+              is_player_one_turn = !is_player_one_turn;
+            }
+
             return;
           }
         }
