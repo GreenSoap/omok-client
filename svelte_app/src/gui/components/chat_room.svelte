7703338976f5f6d2c1d4a23fbf4629066ca323dc
greensoap <william.sjokvist@gmail.com>
2022-06-17 17:48:00 +0200

    Pulled out game logic from view

---
+++ svelte_app/src/gui/components/chat_room.svelte
@@ -0,0 +1,78 @@
+<script>
+	import 'wired-elements';
+	import Fa from 'svelte-fa';
+	import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div>
+	<wired-card>
+		<ol>
+			<li>
+				<article>
+					<header>
+						<figure>
+							<figcaption>William</figcaption>
+						</figure>
+						<time>11:32</time>
+					</header>
+					<p>Hello my name is William xDxD</p>
+				</article>
+			</li>
+			<li>
+				<article>
+					<header>
+						<figure>
+							<figcaption>Joshua</figcaption>
+						</figure>
+						<time>11:32</time>
+					</header>
+					<p>Hello my name is William xDxD</p>
+				</article>
+			</li>
+			<li>
+				<article>
+					<header>
+						<figure>
+							<figcaption>William</figcaption>
+						</figure>
+						<time>11:32</time>
+					</header>
+					<p>Hello my name is Joshua xDxD</p>
+				</article>
+			</li>
+		</ol>
+	</wired-card>
+	<section class="input-area">
+		<wired-input />
+		<wired-button elevation="2">Send <Fa icon={faPaperPlane} /></wired-button>
+	</section>
+</div>
+
+<style lang="scss">
+	div {
+		display: grid;
+		grid-auto-rows: 1fr 0fr;
+	}
+	ol {
+		list-style: none;
+		margin: 5px 20px;
+		padding: 0;
+	}
+
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	figure {
+		margin: 0;
+	}
+
+	.input-area {
+		display: flex;
+		wired-input {
+			flex: 1;
+		}
+	}
+</style>
---
+++ svelte_app/src/gui/omok_game/debug_panel.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import 'wired-elements';
+	import Fa from 'svelte-fa';
+	import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+	export let mouse_coord, piece_coord, player_turn, victory_status;
+
+	let is_open = true;
+	const handle_closer_click = (evt: Event) => {
+		is_open = !is_open;
+	};
+</script>
+
+<wired-card class="debug-panel" elevation="1">
+	<header>
+		<h3>Debug Panel</h3>
+		<wired-button on:click={handle_closer_click}>
+			{#if is_open}
+				<Fa icon={faCaretUp} />
+			{:else}
+				<Fa icon={faCaretDown} />
+			{/if}
+		</wired-button>
+	</header>
+	{#if is_open}
+		<section>
+			<div>
+				<span>Piece: [<em contenteditable="true" bind:textContent={piece_coord} />]</span>
+				<span>Mouse: [<em contenteditable="true" bind:textContent={mouse_coord} />]</span>
+			</div>
+			<div>
+				<span>Player Turn: <strong contenteditable="true" bind:textContent={player_turn} /></span>
+				<span>Victory: <em contenteditable="true" bind:textContent={victory_status} /></span>
+			</div>
+		</section>
+	{/if}
+</wired-card>
+
+<style lang="scss">
+	:global(.debug-panel) {
+		wired-button {
+			background: #ff7979;
+		}
+
+		header {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			font-size: 15px;
+			margin: 0 0 0 5px;
+		}
+
+		div {
+			display: flex;
+			justify-content: space-evenly;
+		}
+
+		h3 {
+			font-size: 15px;
+		}
+	}
+</style>
